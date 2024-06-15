51215d73e6972cb294d293f2a427c5cf7dcb1ee6
aryanthepain <aryan.gupta@iitg.ac.in>
2024-06-15 17:39:11 +0530

    home css unfinished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Navbar from "./navbar";
 import Home from "./Home";
+import Services from "./Services";
+import Question from "./Question";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -10,8 +13,11 @@ function App() {
       <div className="content">
         <Routes>
         <Route index element={<Home />} />
+        <Route path="services" element={<Services />} />
         </Routes>
       </div>
+      <Question />
+      <Footer />
     </div>
     </Router>
   );
--- src/Question.js
+++ src/Question.js
@@ -1,39 +1,37 @@
-import './ques.css';
+// import './ques.css';
 
 const Question = () => {
-    return (
-      <div class="quescontainer">
-        <div class="qtext">
-          <p class="qttitle>Need Consultation?<br>Contact us, we are ready to help
-          </p>
-          <div class="qtcontact">
-            <h3 class="qtctitle">Contact</h3>
-            <ul class="qtccon">
-                <li class="qtcclink qtccl1">JI, Jakarta Bandung, Jakarta</li>
-                <li class="qtcclink qtccl2">021 - 3943943</li>
-                <li class="qtcclink qtccl3">myproperty@gmail.com</li>
-            </ul>
+  return (
+      <div className="quescontainer">
+          <div className="qtext">
+              <p className="qttitle">Need Consultation?<br/>Contact us, we are ready to help</p>
+              <div className="qtcontact">
+                  <h3 className="qtctitle">Contact</h3>
+                  <ul className="qtccon">
+                      <li className="qtcclink qtccl1">JI, Jakarta Bandung, Jakarta</li>
+                      <li className="qtcclink qtccl2">021 - 3943943</li>
+                      <li className="qtcclink qtccl3">myproperty@gmail.com</li>
+                  </ul>
+              </div>
+              <div className="qtcontact qtsocial">
+                  <h3 className="qtctitle">Social Media</h3>
+                  <ul className="qtccon qtcsocial">
+                      <li className="qtcclink qtcs1"><a href="https://www.instagram.com/atp.guptaji/" className="nounder qtcslinka">@myproperty</a></li>
+                      <li className="qtcclink qtcs2"><a href="https://www.linkedin.com/in/aryanthepain/" className="nounder qtcslinka">@myproperty</a></li>
+                      <li className="qtcclink qtcs3"><a href="https://www.instagram.com/atp.guptaji/" className="nounder qtcslinka">My property</a></li>
+                  </ul>
+              </div>
           </div>
-          <div class="qtcontact qtsocial">
-            <h3 class="qtctitle">Social Media</h3>
-            <ul class="qtccon qtcsocial">
-                <li class="qtcclink qtcs1"><a href="https://www.instagram.com/atp.guptaji/" class="nounder qtcslinka">@myproperty</a></li>
-                <li class="qtcclink qtcs2"><a href="https://www.linkedin.com/in/aryanthepain/" class="nounder qtcslinka">@myproperty</a></li>
-                <li class="qtcclink qtcs3"><a href="https://www.instagram.com/atp.guptaji/" class="nounder qtcslinka">My property</a></li>
-            </ul>
+          <div className="quesform">
+              <p className="qftitle">Any Question?</p>
+              <form className="qform">
+                  <input type="email" name="" id="" placeholder="Enter your email here" className="qfemail" />
+                  <input type="text" name="" id="" placeholder="Enter your question here" className="qfques" />
+                  <input type="submit" value="SEND" className="qfsubmit" />
+              </form>
           </div>
-    </div>
-        </div>
-        <div class="quesform">
-        <p class="qftitle">Any Question?</p>
-        <form class="qform">
-            <input type="email" name="" id="" placeholder="Enter your email here" class="qfemail">
-            <input type="text" name="" id="" placeholder="Enter your question here" class="qfques">
-            <input type="submit" value="SEND" class="qfsubmit" >
-        </form>
-        </div>
       </div>
-    );
+  );
 }
- 
+
 export default Question;
